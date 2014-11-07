60df5a601bf22c705212e433ce2c40e039221afa
Bruno Casali <brunoocasali@gmail.com>
2014-11-07 00:46:06 -0200

    Create rename.sh

---
+++ rename.sh
@@ -0,0 +1,5 @@
+
+# Used when you want to remove some string in your archives downloaded from plixid.com (best site ever!)
+rename -v 's/_\[plixid\.com\]//' *.mp3
+
+# Output your files that be matched by the perl regxp.! :D
