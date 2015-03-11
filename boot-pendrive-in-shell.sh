2aee014bbe2660ee7a3ea5f9d990969baaa64a5b
Bruno Casali <brunoocasali@gmail.com>
2015-03-10 23:30:56 -0300

    Update boot-pendrive-in-shell.sh

--- boot-pendrive-in-shell.sh
+++ boot-pendrive-in-shell.sh
@@ -1,3 +1,8 @@
+# First you'll need to see what devices is mounted in your computer.
+$ mount
+# Second you'll need to umount this devices with:
+$ umount /dev/sdb1 # for example
+
 # Where is the 'memtest.img' is the image what you want to write 
 # and the 'sd' is the device destination
-dd if=memtest.img of=/dev/sd?
+$ sudo dd if=memtest.img of=/dev/sd?
