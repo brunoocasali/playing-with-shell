1f48dae55d7e5f568516cbfa51302d0f9a287c3d
Bruno Casali <brunoocasali@gmail.com>
2015-05-07 14:33:22 -0300

    Create git-bashrc.sh

---
+++ git-bashrc.sh
@@ -0,0 +1,10 @@
+# There are my github aliases to improve my typing every day!
+
+# Git Aliases
+
+alias gp='git push'
+alias gpl='git pull'
+alias gc='git commit -m'
+alias ga='git add .'
+alias gst='git status'
+alias gb='git branch'
