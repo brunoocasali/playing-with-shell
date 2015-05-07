1fcbd8525c9a2aeb5863b8d6807b9b150dd0decf
Bruno Casali <brunoocasali@gmail.com>
2015-05-07 14:36:15 -0300

    Update git-bashrc.sh

--- git-bashrc.sh
+++ git-bashrc.sh
@@ -2,6 +2,10 @@
 
 # Git Aliases
 
+# Run these commands to use only inside current branch
+# $ git config --global push.default current
+# $ git config --global pull.default current
+
 alias gp='git push'
 alias gpl='git pull'
 alias gc='git commit -m'
