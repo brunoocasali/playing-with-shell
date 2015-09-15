0cb23294cae6118ec82ef44f2556d7646e1f6ae2
Bruno Casali <brunoocasali@gmail.com>
2015-09-15 09:16:46 -0300

    Update find-and-destroy.sh

--- find-and-destroy.sh
+++ find-and-destroy.sh
@@ -1,3 +1,5 @@
 # Say: never more! to your crazy dependencies on maven! :D
 
- find ~/.m2  -name "*.lastUpdated" -exec grep -q "Could not transfer" {} \; -print -exec rm {} \;
+find ~/.m2  -name "*.lastUpdated" -exec grep -q "Could not transfer" {} \; -print -exec rm {} \;
+
+git rm --cached `git ls-files -i --exclude-from=.gitignore`
