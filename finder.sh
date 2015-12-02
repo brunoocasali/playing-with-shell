9dfd1e244a18dcd632cbed834fcc72ee77ce930a
Bruno Casali <brunoocasali@gmail.com>
2015-12-01 23:58:17 -0200

    Update finder.sh

--- finder.sh
+++ finder.sh
@@ -1,1 +1,12 @@
 cd $(find /home/ -name DIR-NAME 2>/dev/null)
+
+# Just place this in your .bashrc
+
+finderx() {
+  cd $(find /home/ -name $1 2>/dev/null)
+}
+alias cec=finderx
+
+# and use:
+
+cec dirname
